847caec76e0c7f31a92eef09bb754983a095d240
amosxrl <amosxrl@outlook.com>
2025-02-14 15:19:27 -0300

    Primeiro commit: adicionar os arquivos java que pertencia ao estudo para o estudoJava

---
+++ Locadora de Veiculos/src/model/LocadoraVeiculos.java
@@ -0,0 +1,9 @@
+package model;
+import java.util.List;
+
+public class LocadoraVeiculos {
+    private List<Veiculos> veiculosDisponiveis;
+    private List<Clientes> clientes;
+    private List<Aluguel> alugueis;
+    // outros atributos relevantes
+}
