3a800f085c236955f35dbd6d2fe8c4d21c4ed464
amosxrl <amosxrl@outlook.com>
2025-02-21 19:47:23 +0000

    Salvando variaveis da aula11 de loiane

---
+++ src/main/java/com/loianegroner/curso/aula11/VariaveisBoolean.java
@@ -0,0 +1,9 @@
+package com.loianegroner.curso.aula11;
+
+public class VariaveisBoolean {
+    public static void main(String[] args) {
+        boolean ver = true;
+        boolean fal = false;
+        System.out.println("O valor verdadeiro é " + ver);
+    }
+}
---
+++ src/main/java/com/loianegroner/curso/aula11/VariaveisChar.java
@@ -0,0 +1,10 @@
+package com.loianegroner.curso.aula11;
+
+public class VariaveisChar {
+    public static void main(String[] args) {
+        char o = 111;
+        char i = 105;
+        char interrogacao = 0X00E1; // valor ?
+        System.out.println(""+o+i+interrogacao);
+    }
+}
