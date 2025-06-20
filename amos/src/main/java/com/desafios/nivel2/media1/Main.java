a00fec529a2ad6200a7b4358317cd62edd3c454e
amos <amosxrl@outlook.com>
2025-06-20 16:38:25 -0300

    Mudanças inrelevantes

--- amos/src/main/java/com/desafios/nivel2/media1/Main.java
+++ amos/src/main/java/com/desafios/nivel2/media1/Main.java
@@ -2,9 +2,9 @@ package com.desafios.nivel2.media1;
 
 import java.io.IOException;
 import java.util.Scanner;
- 
+
 /**
- * IMPORTANT: 
+ * IMPORTANT:
  *      O nome da classe deve ser "Main" para que a sua solução execute
  *      Class name must be "Main" for your solution to execute
  *      El nombre de la clase debe ser "Main" para que su solución ejecutar
--- amos/src/main/java/com/desafios/nivel5/olamundo/helloWould.java
+++ amos/src/main/java/com/desafios/nivel5/olamundo/helloWould.java
@@ -1,6 +1,6 @@
 package com.desafios.nivel5.olamundo;
 import java.io.IOException;
- 
+
 /**
  * IMPORTANT: 
  *      O nome da classe deve ser "Main" para que a sua solução execute
@@ -8,9 +8,9 @@ import java.io.IOException;
  *      El nombre de la clase debe ser "Main" para que su solución ejecutar
  */
 public class helloWould{
- 
+
     public static void main(String[] args) throws IOException {
- 
+
         /**
          * Escreva a sua solução aqui
          * Code your solution here
@@ -19,5 +19,5 @@ public class helloWould{
         System.out.println("Hello, World!");
 
     }
- 
+
 }
